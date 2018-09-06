429c296bfb55c32104804669edade6e227549b30
Konstantin Savenkov <konctantin.grigorievich@gmail.com>
2018-09-07 01:52:31 +0300

    clean up

--- apiserver.go
+++ apiserver.go
@@ -4,18 +4,9 @@ import (
 	"github.com/KonstantinGrig/goapisql/config"
 	"github.com/KonstantinGrig/goapisql/handlers"
 	"github.com/valyala/fasthttp"
-	"log"
-	"os"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) > 0 {
-		log.Println("Start with custom config file: " + args[0])
-	} else {
-		log.Println("Start default config file: config.json!")
-	}
-	os.Setenv("GOAPISQL_ENV", "test")
 	config.Init()
 	port := config.GetString("server-port")
 
